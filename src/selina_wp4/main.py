6773276696cafe0a6972cbf4cfc46fc580382a54
Niccolo Cantu <niccolo.cantu@nina.no>
2026-03-09 10:12:39 +0100

    Revert "chg: remove unused code"
    This reverts commit 628a55de3a6e2300dab0d6f8c923fe21f0aff0bc.

--- src/selina_wp4/main.py
+++ src/selina_wp4/main.py
@@ -3,6 +3,7 @@ import io
 import json
 import logging
 import pathlib
+from collections import defaultdict
 from typing import Any
 
 import environ
@@ -15,6 +16,7 @@ from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from mistune.directives import Admonition, RSTDirective, TableOfContents
+from slugify import slugify
 
 from .directives import Accordion, Join
 
@@ -63,8 +65,88 @@ SURVEY_CONFIG = json.load((BASE_DIR / "static" / "survey-config.json").open("r")
 log.debug("using survey config", config=SURVEY_CONFIG)
 
 
-def get_markdown(content: str) -> str:
+SURVEY_DEBUG_PATH = BASE_DIR / "survey.json" if MODE == "dev" else None
+
+STOP_WORDS = [
+    "What",
+    "is",
+    "are",
+    "which",
+    "should",
+    "the",
+    "a",
+    "an",
+    "of",
+    "in",
+    "for",
+    "as",
+    "s",
+    "if",
+    "or",
+    "it",
+    "she",
+    "he",
+    "all",
+    "to",
+    "this",
+    "that",
+    "these",
+    "those",
+    "at",
+    "and",
+    "how",
+    "e",
+    "g",
+    "i",
+    "be",
+]
+
+
+class Slugifier:
+    def __init__(self):
+        self.__ids = defaultdict(lambda: 0)
+
+    def run(self, *args, **kwargs):
+        result = slugify(*args, **kwargs)
+
+        self.__ids[result] += 1
+        return (
+            result
+            if self.__ids.get(result) < 2
+            else f"{result}-{self.__ids.get(result)}"
+        )
+
+
+class CustomRenderer(mistune.HTMLRenderer):
+    def __init__(
+        self,
+        slugifier: Slugifier,
+        escape: bool = True,
+        debug=False,
+        page_name: str = "",
+    ):
+        super().__init__(escape=False)
+        self.slugifier = slugifier
+        self.debug = debug
+        self.page_name = page_name
+
+    def heading(self, text: str, level: int, **attrs: Any) -> str:
+        header_id = attrs.get("id") or self.slugifier.run(
+            text,
+            stopwords=STOP_WORDS,
+            lowercase=True,
+            word_boundary=True,
+            max_length=50,
+        )
+        if self.debug:
+            return f'<div class="flex flex-col gap-4"><h{level} id="{header_id}">{text}</h{level}><small>/{self.page_name}#{header_id}</small></div>\n'  # noqa: E501
+        return f'<h{level} id="{header_id}">{text}</h{level}>\n'
+
+
+def get_markdown(content: str, debug: bool = False, page_name: str = "") -> str:
+    slugifier = Slugifier()
     return mistune.create_markdown(
+        renderer=CustomRenderer(slugifier=slugifier, debug=debug, page_name=page_name),
         plugins=[
             "footnotes",
             "url",
@@ -77,9 +159,32 @@ def get_markdown(content: str) -> str:
     )(content)
 
 
+if SURVEY_DEBUG_PATH:
+
+    @app.get("/survey-debug")
+    def preview(request: Request):
+        if not SURVEY_DEBUG_PATH.exists():
+            return "create a file survey.json in the root of the project with the data you want to debug"  # noqa: E501
+
+        with SURVEY_DEBUG_PATH.open("r") as f:
+            survey = templates.get_template("survey-render.md.jinja").render(
+                json.load(f)
+            )
+
+        return templates.TemplateResponse(
+            request=request,
+            name="pages.html.jinja",
+            context={"content": get_markdown(survey)},
+        )
+
+
 @app.post("/submit")
 def submit(request_body: dict[Any, Any]):
     log.debug("submitted", data=request_body)
+
+    if SURVEY_DEBUG_PATH:
+        with SURVEY_DEBUG_PATH.open("w") as f:
+            json.dump(request_body, f)
 
     survey = templates.get_template("survey-render.md.jinja").render(**request_body)
 
@@ -107,7 +212,10 @@ async def index(request: Request, page_name: str, debug: bool = False):
         content = get_markdown(
             (WEBSITE_PATH / (pathlib.Path(page_name).with_suffix(".md")))
             .open("r")
-            .read()
+            .read(),
+            # Use attrs from processed tokens if available
+            debug=debug,
+            page_name=page_name,
         )
         return templates.TemplateResponse(
             request=request, name="pages.html.jinja", context={"content": content}
